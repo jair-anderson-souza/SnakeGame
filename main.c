6b6107139c6bef2625ad79d7b4c7d7cfd40f9d09
Anderson Souza <jair_anderson_bs@hotmail.com>
2018-03-29 01:05:12 -0300

    change macros

--- main.c
+++ main.c
@@ -1,25 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.c
  * Author: Anderson Souza <jair_anderson_bs@hotmail.com>
  *
  * Created on March 27, 2018, 7:32 PM
  */
-#include <stdlib.h>
-#include <stdio.h>
-#include <ncurses.h>
 #include "screen.h"
 #include "list.h"
 
-
-
 int main(int argc, char** argv) {
-
     init_mode_cursor();
     int key = default_dir;
     bool next = TRUE;
